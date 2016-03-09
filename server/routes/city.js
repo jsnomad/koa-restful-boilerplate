f92f592b0943562f302c14cbe636df64e8c421f6
Thomas Blanc-Hector <thomas.blanc.hector@gmail.com>
2016-03-09 23:14:44 +0100

    error handling
    error handling

--- server/routes/city.js
+++ server/routes/city.js
@@ -11,21 +11,67 @@ router.prefix(`/${baseApi}/${api}`)
 
 /* eslint-disable no-unused-vars, no-param-reassign, new-cap */
 
-// GET /api
-router.get('/', async(ctx, next) =>
-    ctx.body = await City.find())
-// POST /api
-router.post('/', async(ctx, next) =>
-    ctx.body = await new City(ctx.request.body).save())
-// Get /api/id
-router.get('/:id', async(ctx, next) =>
-    ctx.body = await City.findById(ctx.params.id))
-// PUT /api/id
-router.put('/:id', async(ctx, next) =>
-    ctx.body = await City.findByIdAndUpdate(ctx.params.id, ctx.request.body))
-// DELETE /api/id
-router.delete('/:id', async(ctx, next) =>
-  ctx.body = await City.findByIdAndRemove(ctx.params.id))
+// GET /api/city
+router.get('/', async(ctx) =>
+  ctx.body = await City.find())
+
+// POST /api/city
+router.post('/', async(ctx) => {
+  try {
+    const city = await new City(ctx.request.body).save()
+    ctx.body = city
+  } catch (err) {
+    ctx.throw(422)
+  }
+})
+
+// GET /api/city/id
+router.get('/:id', async(ctx) => {
+  try {
+    const city = await City.findById(ctx.params.id)
+    if (!city) {
+      ctx.throw(404)
+    }
+    ctx.body = city
+  } catch (err) {
+    if (err.name === 'CastError' || err.name === 'NotFoundError') {
+      ctx.throw(404)
+    }
+    ctx.throw(500)
+  }
+})
+
+// PUT /api/city/id
+router.put('/:id', async(ctx) => {
+  try {
+    const city = await City.findByIdAndUpdate(ctx.params.id, ctx.request.body)
+    if (!city) {
+      ctx.throw(404)
+    }
+    ctx.body = city
+  } catch (err) {
+    if (err.name === 'CastError' || err.name === 'NotFoundError') {
+      ctx.throw(404)
+    }
+    ctx.throw(500)
+  }
+})
+
+// DELETE /api/city/id
+router.delete('/:id', async(ctx) => {
+  try {
+    const city = await City.findByIdAndRemove(ctx.params.id)
+    if (!city) {
+      ctx.throw(404)
+    }
+    ctx.body = city
+  } catch (err) {
+    if (err.name === 'CastError' || err.name === 'NotFoundError') {
+      ctx.throw(404)
+    }
+    ctx.throw(500)
+  }
+})
 
 /* eslint-enable no-unused-vars, no-param-reassign, new-cap */
 
