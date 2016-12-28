a653a2070b57ad9cf3c528f48ef5d8676ecf4cc5
Thomas Blanc-Hector <thomas.blanc.hector@gmail.com>
2016-12-28 13:26:47 +0700

    add cities controller
    add cities controller

---
+++ server/controllers/cities.js
@@ -0,0 +1,98 @@
+import 'babel-polyfill'
+import Router from 'koa-router'
+import City from '../models/cities'
+import { baseApi } from '../config'
+
+const api = 'city'
+
+const router = new Router();
+
+router.prefix(`/${baseApi}/${api}`)
+
+class CitiesControllers {
+
+  /* eslint-disable no-param-reassign*/
+
+  /**
+   * Get all cities
+   * @param {ctx} Koa Context
+   */
+  async find(ctx) {
+    ctx.body = await City.find()
+  }
+
+  /**
+   * Find a city
+   * @param {ctx} Koa Context
+   */
+  async findById(ctx) {
+    try {
+      const city = await City.findById(ctx.params.id)
+      if (!city) {
+        ctx.throw(404)
+      }
+      ctx.body = city
+    } catch (err) {
+      if (err.name === 'CastError' || err.name === 'NotFoundError') {
+        ctx.throw(404)
+      }
+      ctx.throw(500)
+    }
+  }
+
+  /**
+   * Add a city
+   * @param {ctx} Koa Context
+   */
+  async add(ctx) {
+    try {
+      const city = await new City(ctx.request.body).save()
+      ctx.body = city
+    } catch (err) {
+      ctx.throw(422)
+    }
+  }
+
+  /**
+   * Update a city
+   * @param {ctx} Koa Context
+   */
+  async update(ctx) {
+    try {
+      const city = await City.findByIdAndUpdate(ctx.params.id, ctx.request.body)
+      if (!city) {
+        ctx.throw(404)
+      }
+      ctx.body = city
+    } catch (err) {
+      if (err.name === 'CastError' || err.name === 'NotFoundError') {
+        ctx.throw(404)
+      }
+      ctx.throw(500)
+    }
+  }
+
+  /**
+   * Delete a city
+   * @param {ctx} Koa Context
+   */
+  async delete(ctx) {
+    try {
+      const city = await City.findByIdAndRemove(ctx.params.id)
+      if (!city) {
+        ctx.throw(404)
+      }
+      ctx.body = city
+    } catch (err) {
+      if (err.name === 'CastError' || err.name === 'NotFoundError') {
+        ctx.throw(404)
+      }
+      ctx.throw(500)
+    }
+  }
+
+  /* eslint-enable no-param-reassign */
+
+}
+
+export default new CitiesControllers()
