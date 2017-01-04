830e529fd6285e8f6fbcd153e539396fbf068bbd
Thomas Blanc-Hector <thomas.blanc.hector@gmail.com>
2017-01-04 23:30:14 +0700

    beautify code
    beautify code

--- server/models/cities.js
+++ server/models/cities.js
@@ -2,12 +2,23 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+mongoose.Promise = global.Promise;
+
 const citySchema = new Schema({
-  name: { type: String, required: true },
-  totalPopulation: { type: Number, required: true },
+  name: {
+    type: String,
+    required: true,
+  },
+  totalPopulation: {
+    type: Number,
+    required: true,
+  },
   country: String,
   zipCode: Number,
-  updated: { type: Date, default: Date.now },
+  updated: {
+    type: Date,
+    default: Date.now,
+  },
 })
 
 export default mongoose.model('City', citySchema)
