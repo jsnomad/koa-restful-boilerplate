29031d40dee2f145038ad7cbd5be057663ed5e31
Thomas Blanc-Hector <thomas.blanc.hector@gmail.com>
2017-01-04 23:05:51 +0700

    add Helmet
    Helmet helps secure your Express apps by setting various HTTP headers

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import bodyParser from 'koa-bodyparser'
 import Koa from 'koa'
 import logger from 'koa-logger'
 import mongoose from 'mongoose'
+import helmet from 'koa-helmet'
 import routing from './routes/'
 import { port, connexionString } from './config'
 
@@ -14,6 +15,7 @@ const app = new Koa()
 app
   .use(logger())
   .use(bodyParser())
+  .use(helmet())
 
 routing(app)
 
